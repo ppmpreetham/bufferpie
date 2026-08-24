ed76a81c42607d8cbb8ef5144c101e9a00ea9266
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-08-24 12:55:12 +0530

    add: compilation flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+    all(target_os = "windows", not(debug_assertions)),
+    windows_subsystem = "windows"
+)]
+
 mod app;
 mod key;
 mod startup;
@@ -6,6 +11,8 @@ mod ui;
 use gpui::Application;
 
 fn main() {
+    #[cfg(not(debug_assertions))]
     startup::register_startup();
+
     Application::new().run(app::run);
 }
