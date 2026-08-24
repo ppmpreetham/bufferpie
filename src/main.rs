d59345a5e67a4ab6f3f16f045ae4d3bd725d5dea
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-08-24 11:06:47 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+
+use gpui::{
+    App, Application, Bounds, Context, SharedString, Window, WindowBounds, WindowOptions, div,
+    prelude::*, px, rgb, size,
+};
+
+fn main() {
+    Application::new().run(|cx: &mut App| {
+        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |_, cx| {
+                cx.new(|_| HelloWorld {
+                    text: "World".into(),
+                })
+            },
+        )
+        .unwrap();
+    });
+}
