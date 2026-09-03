eb32670ea909462aea17fec546b97a8b0cbf4cab
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-03 19:03:17 +0530

    perf: remove hardcoded key

--- src/actions/keystrokes.rs
+++ src/actions/keystrokes.rs
@@ -3,84 +3,30 @@ use std::thread::sleep;
 use std::time::Duration;
 
 /// readable label for a captured key
-pub fn key_label(key: Key) -> &'static str {
+use std::borrow::Cow;
+
+pub fn key_label(key: &Key) -> Cow<'static, str> {
     match key {
-        Key::Alt => "alt",
-        Key::AltGr => "altgr",
-        Key::Backspace => "backspace",
-        Key::CapsLock => "caps",
-        Key::ControlLeft | Key::ControlRight => "ctrl",
-        Key::Delete => "del",
-        Key::DownArrow => "down",
-        Key::End => "end",
-        Key::Escape => "esc",
-        Key::F1 => "f1",
-        Key::F2 => "f2",
-        Key::F3 => "f3",
-        Key::F4 => "f4",
-        Key::F5 => "f5",
-        Key::F6 => "f6",
-        Key::F7 => "f7",
-        Key::F8 => "f8",
-        Key::F9 => "f9",
-        Key::F10 => "f10",
-        Key::F11 => "f11",
-        Key::F12 => "f12",
-        Key::Home => "home",
-        Key::LeftArrow => "left",
-        Key::MetaLeft | Key::MetaRight => "meta",
-        Key::PageDown => "pagedown",
-        Key::PageUp => "pageup",
-        Key::Return => "enter",
-        Key::RightArrow => "right",
-        Key::ShiftLeft | Key::ShiftRight => "shift",
-        Key::Space => "space",
-        Key::Tab => "tab",
-        Key::UpArrow => "up",
-        Key::PrintScreen => "printscr",
-        Key::ScrollLock => "scrolllock",
-        Key::Pause => "pause",
-        Key::NumLock => "numlock",
-        Key::BackQuote => "`",
-        Key::Minus => "-",
-        Key::Equal => "=",
-        Key::Num1 => "1",
-        Key::Num2 => "2",
-        Key::Num3 => "3",
-        Key::Num4 => "4",
-        Key::Num5 => "5",
-        Key::Num6 => "6",
-        Key::Num7 => "7",
-        Key::Num8 => "8",
-        Key::Num9 => "9",
-        Key::Num0 => "0",
-        Key::KeyA => "a",
-        Key::KeyB => "b",
-        Key::KeyC => "c",
-        Key::KeyD => "d",
-        Key::KeyE => "e",
-        Key::KeyF => "f",
-        Key::KeyG => "g",
-        Key::KeyH => "h",
-        Key::KeyI => "i",
-        Key::KeyJ => "j",
-        Key::KeyK => "k",
-        Key::KeyL => "l",
-        Key::KeyM => "m",
-        Key::KeyN => "n",
-        Key::KeyO => "o",
-        Key::KeyP => "p",
-        Key::KeyQ => "q",
-        Key::KeyR => "r",
-        Key::KeyS => "s",
-        Key::KeyT => "t",
-        Key::KeyU => "u",
-        Key::KeyV => "v",
-        Key::KeyW => "w",
-        Key::KeyX => "x",
-        Key::KeyY => "y",
-        Key::KeyZ => "z",
-        _ => "?",
+        Key::ControlLeft | Key::ControlRight => Cow::Borrowed("ctrl"),
+        Key::MetaLeft | Key::MetaRight => Cow::Borrowed("meta"),
+        Key::ShiftLeft | Key::ShiftRight => Cow::Borrowed("shift"),
+        Key::Return | Key::KpReturn => Cow::Borrowed("enter"),
+        Key::CapsLock => Cow::Borrowed("caps"),
+        Key::Delete | Key::KpDelete => Cow::Borrowed("del"),
+        Key::PrintScreen => Cow::Borrowed("printscr"),
+        Key::Unknown(code) => Cow::Owned(format!("unknown_{}", code)),
+
+        other => {
+            let raw_name = format!("{:?}", other);
+            let cleaned = if let Some(stripped) = raw_name.strip_prefix("Key") {
+                stripped.to_lowercase()
+            } else if let Some(stripped) = raw_name.strip_prefix("Num") {
+                stripped.to_lowercase()
+            } else {
+                raw_name.to_lowercase()
+            };
+            Cow::Owned(cleaned)
+        }
     }
 }
 
